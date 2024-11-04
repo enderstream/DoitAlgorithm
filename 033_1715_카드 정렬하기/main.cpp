a34a90a56ab1f9b03f08ce75d29b8f1b1e02a68f
enderstream00 <enderstream00@naver.com>
2024-11-04 15:33:50 +0900

    .

---
+++ 033_1715_카드 정렬하기/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+
+#ifdef BOJ // 백준 채점환경
+constexpr bool local = false;
+#else // 로컬
+constexpr bool local = true;
+#endif
+
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
+#define debug            \
+    if constexpr (local) \
+    std::cout
+#define endl "\n"
+
+using namespace std;
+
+typedef priority_queue<int, vector<int>, greater<int>> min_heap;
+
+void solution(int N, min_heap &card_deck);
+
+int main(int argc, char const *argv[])
+{
+    FAST_IO;
+    if constexpr (local)
+        (void)!freopen("./input.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+
+    min_heap card_deck;
+    for (int i = 1; i <= N; i++)
+    {
+        int card;
+        cin >> card;
+        card_deck.push(card);
+    }
+
+    solution(N, card_deck);
+    return 0;
+}
+
+void solution(int N, min_heap &card_deck)
+{
+    int total_compare = 0;
+    while (card_deck.size() > 1)
+    {
+        int deck1 = card_deck.top();
+        card_deck.pop();
+        int deck2 = card_deck.top();
+        card_deck.pop();
+
+        total_compare += deck1 + deck2;
+
+        card_deck.push(deck1 + deck2);
+    }
+    cout << total_compare;
+}
---
+++ 034_1744_수를 묶어서 최댓값 만들기/main.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#ifdef BOJ // 백준 채점환경
+constexpr bool local = false;
+#else // 로컬
+constexpr bool local = true;
+#endif
+
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
+#define debug            \
+    if constexpr (local) \
+    std::cout
+#define endl "\n"
+
+using namespace std;
+
+void solution(bool zero, int P, int N, vector<int> &pos, vector<int> &neg);
+
+int main(int argc, char const *argv[])
+{
+    FAST_IO;
+    if constexpr (local)
+        (void)!freopen("./input.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+    vector<int> pos;
+    vector<int> neg;
+    bool zero = false;
+    for (int i = 0; i < N; i++)
+    {
+        int number;
+        cin >> number;
+        if (number > 0)
+            pos.push_back(number);
+        else if (number < 0)
+            neg.push_back(number);
+        else
+            zero = true;
+    }
+
+    sort(pos.begin(), pos.end(), greater<int>()); // 양수는 내림차순
+    sort(neg.begin(), neg.end());                 // 음수는 오름차순
+
+    solution(zero, pos.size(), neg.size(), pos, neg);
+    return 0;
+}
+
+void solution(bool zero, int P, int N, vector<int> &pos, vector<int> &neg)
+{
+    int sum = 0;
+
+    // 양수 먼저 계산
+    // 1부터 처리
+    for (int i = P-1; i >= 0; i--)
+        if(pos[i] == 1)
+            sum++;
+            
+    // 전체 양수 개수 중 1개수 제외
+    P-= sum;
+    for (int i = 0; i <= P - 2; i += 2)
+        sum += pos[i] * pos[i + 1];
+    if (P % 2 == 1)
+        sum += pos[P - 1];
+
+    // 음수 계산
+    for (int i = 0; i <= N - 2; i += 2)
+        sum += neg[i] * neg[i + 1];
+    if (!zero && N % 2 == 1)
+        sum += neg[N - 1];
+
+    cout << sum;
+}
