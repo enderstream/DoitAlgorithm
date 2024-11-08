abb3ec6422595da832353ab5fd454dd657069ff4
enderstream00 <enderstream00@naver.com>
2024-11-08 16:48:47 +0900

    .

--- 046_18352_특정 거리의 도시 찾기/main.cpp
+++ 046_18352_특정 거리의 도시 찾기/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <unordered_set>
+#include <algorithm>
 
 #ifdef BOJ // 백준 채점환경
 constexpr bool local = false;
@@ -17,7 +21,10 @@ constexpr bool local = true;
 
 using namespace std;
 
-void solution();
+typedef vector<unordered_set<int>> vector_set;
+
+void solution(int N, int K, int X, vector_set &city);
+vector<int> BFS(int N, int K, int X, vector_set &city);
 
 int main(int argc, char const *argv[])
 {
@@ -26,13 +33,77 @@ int main(int argc, char const *argv[])
     if constexpr (local)
         (void)!freopen("./input.txt", "r", stdin);
 
-    // io here
-    // debug << "Hello World!" << endl;
+    int N, M, K, X;
+    cin >> N >> M >> K >> X;
 
-    solution();
+    vector_set city(N + 1);
+
+    for (int i = 0; i < M; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+        city[u].insert(v);
+    }
+
+    solution(N, K, X, city);
     return 0;
 }
 
-void solution()
+void solution(int N, int K, int X, vector_set &city)
 {
+    vector<int> dist = BFS(N, K, X, city);
+    vector<int> K_dist;
+    for (int i = 1; i <= N; i++)
+        if (dist[i] == K)
+            K_dist.push_back(i);
+
+    if (K_dist.size())
+    {
+        sort(K_dist.begin(), K_dist.end());
+        for (auto node : K_dist)
+            cout << node << "\n";
+    }
+    else
+        cout << -1;
 }
+
+vector<int> BFS(int N, int K, int X, vector_set &city)
+{
+    vector<bool> visited(N + 1, false);
+    vector<int> dist(N + 1, 0);
+    visited[X] = true;
+    queue<int> Q({X});
+
+    while (!Q.empty())
+    {
+        int curr = Q.front();
+        Q.pop();
+
+        for (int next = 1; next <= N; next++)
+        {
+            if (visited[next] || city[curr].find(next) == city[curr].end())
+                continue;
+
+            dist[next] = dist[curr] + 1;
+
+            if (dist[next] > K)
+                return dist;
+
+            visited[next] = true;
+            Q.push(next);
+        }
+    }
+    return dist;
+}
+
+// typedef unordered_map<int, unordered_map<int,int>> nested_dict;
+// bool safe_access(nested_dict &city, int &curr, int &next)
+// {
+//     auto it_outer = city.find(curr);
+//     if (it_outer == city.end())
+//         return false;
+//     auto it_inner = it_outer->second.find(next);
+//     if (it_inner == it_outer->second.end())
+//         return false;
+//     return true;
+// }
