30edb84c83482f5a76794cbd934bf1278b548e51
enderstream00 <enderstream00@naver.com>
2025-06-13 16:10:43 +0900

    084 clear

---
+++ 084_1463_정수를 1로 만들기/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#ifdef BOJ // 백준 채점환경
+constexpr bool local = false;
+#else // 로컬
+constexpr bool local = true;
+#endif
+
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
+#define debug            \
+    if constexpr (local) \
+    std::cout
+#define endl "\n"
+
+using namespace std;
+
+void solution(int N);
+
+int main(int argc, char const *argv[])
+{
+    FAST_IO;
+    if constexpr (local)
+        (void)!freopen("./input.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+
+    solution(N);
+    return 0;
+}
+
+void solution(int N)
+{
+    vector<int> DP(N + 1, N + 1);
+    DP[1] = DP[0] = 0;
+    for (int i = 1; i < N; i++)
+    {
+        DP[i + 1] = min(DP[i] + 1, DP[i + 1]);
+        if (i * 2 <= N) DP[i * 2] = min(DP[i] + 1, DP[i * 2]);
+        if (i * 3 <= N) DP[i * 3] = min(DP[i] + 1, DP[i * 3]);
+    }
+    cout << DP[N];
+}
