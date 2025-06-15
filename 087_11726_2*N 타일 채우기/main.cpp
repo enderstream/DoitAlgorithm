8a436b50091c4464488cc977c9716f7ab350df4f
enderstream00 <enderstream00@naver.com>
2025-06-15 20:32:31 +0900

    087 clear

---
+++ 087_11726_2*N 타일 채우기/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#ifdef BOJ // 백준 채점환경
+constexpr bool local = false;
+#else // 로컬
+constexpr bool local = true;
+#endif
+
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
+#define debug            \
+    if constexpr (local) \
+    std::cout
+#define endl "\n"
+
+#define MOD 10007
+
+using namespace std;
+
+typedef unsigned long long ui64;
+
+void solution(int N);
+
+int main(int argc, char const *argv[])
+{
+    FAST_IO;
+    if constexpr (local)
+        (void)!freopen("./input.txt", "r", stdin);
+
+    int N;
+    cin >> N;
+
+    solution(N);
+    return 0;
+}
+
+void solution(int N)
+{
+    // 전형적인 피보나치
+    int A = 0, B = 1, C = 1;
+    for (int i = 2; i <= N; i++)
+    {
+        A = B;
+        B = C;
+        C = (B + A) % MOD;
+    }
+    cout << C;
+}
