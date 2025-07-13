92cf62d48229e14a0f2c061203bb51d684a76147
enderstream00 <enderstream00@naver.com>
2025-07-13 20:02:58 +0900

    097 clear

---
+++ 097_11758_선분 방향 구하기/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#ifdef BOJ // 백준 채점환경
+constexpr bool local = false;
+#else // 로컬
+constexpr bool local = true;
+#endif
+
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
+#define debug            \
+    if constexpr (local) \
+    std::cout
+#define endl "\n"
+
+using namespace std;
+
+typedef struct point
+{
+    int x;
+    int y;
+} point_t;
+
+void solution(point_t P1, point_t P2, point_t P3);
+
+int main(int argc, const char *argv[])
+{
+    FAST_IO;
+    if constexpr (local)
+        (void)!freopen("./input.txt", "r", stdin);
+
+    point_t P1, P2, P3;
+    cin >> P1.x >> P1.y >> P2.x >> P2.y >> P3.x >> P3.y;
+
+    solution(P1, P2, P3);
+    return 0;
+}
+
+void solution(point_t P1, point_t P2, point_t P3)
+{
+    int CCW = P1.x * P2.y + P2.x * P3.y + P3.x * P1.y - P2.x * P1.y - P3.x * P2.y - P1.x * P3.y;
+    if (CCW < 0)
+        cout << -1;
+    else if (CCW > 0)
+        cout << 1;
+    else
+        cout << 0;
+}
